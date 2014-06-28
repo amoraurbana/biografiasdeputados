3688d8779563463c5af7645a2226d8c3417cda6a
amoraurbana <>
2014-06-28 16:00:47 -0300

    listagem deps

--- wsgi/myflaskapp.py
+++ wsgi/myflaskapp.py
@@ -1,16 +1,38 @@
+import xml.etree.ElementTree as ET
 from flask import Flask
 app = Flask(__name__)
 
+
+raiz = ET.parse("static/bios/BIOGRAFIA 0 - 700.xml")
+deps = raiz.findall("./DATA_RECORD")
+D = {}
+for dep in deps:
+    cod = dep.find("CODPESSOA").text
+    crian = dep.getchildren()
+    d = {}
+    for c in crian:
+        d[c.tag] = c.text
+    D[cod] = d
+    
+print D
+
+
 @app.route("/")
-def hello():
-    return "melhorar!"
+def todos():
+    pag = ""
+    for dep in D.values():
+        pag += dep["TXTNOME"]
+    return pag
 
 
 
 
 @app.route("/bethoven")
 def bebe():
-    return "<h1>Bethoven!</h1>"
+    return '<h1>Bethoven!</h1><img src="static/midia/bethoven.png">'
+
+
+
 
 
 
