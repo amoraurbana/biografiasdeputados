37876738e20b9cbde5a97b2686288d185c72029d
amoraurbana <au>
2014-06-29 11:37:10 -0300

    funciona com fotos

--- wsgi/myflaskapp.py
+++ wsgi/myflaskapp.py
@@ -1,28 +1,46 @@
+import os 
 import xml.etree.ElementTree as ET
 from flask import Flask
 app = Flask(__name__)
 
-diretorio = __file__.rpartition("/")[0]
-raiz = ET.parse(diretorio+"/static/bios/0.xml")
-deps = raiz.findall("./DATA_RECORD")
-
 D = {}
-for dep in deps:
-    cod = dep.find("CODPESSOA").text
-    crian = dep.getchildren()
-    d = {}
-    for c in crian:
-        d[c.tag] = c.text
-    D[cod] = d
+diretorio = __file__.rpartition("/")[0]
+diretorioxmls = diretorio+"/static/bios/"
+xmls = os.listdir(diretorioxmls)
+for xml in xmls:
+    raiz = ET.parse(diretorioxmls+xml)
+    deps = raiz.findall("./DATA_RECORD")
+
+    for dep in deps:
+        cod = dep.find("CODPESSOA").text
+        crian = dep.getchildren()
+        d = {}
+        for c in crian:
+            d[c.tag] = c.text
+        D[cod] = d
     
-print D
+print "processados!"
+#print D
 
 
 @app.route("/")
 def todos():
-    pag = ""
-    for dep in D.values():
-        pag += dep["TXTNOME"]
+    pag = "<ul>"
+    for cod, dep in D.items():
+        nome = dep["TXTNOME"]
+        pag += '<li><a href="dep/%s">%s</a></li>' % (cod, nome)
+    pag += "</ul>"   
+    return pag
+    
+@app.route("/dep/<cod>")
+def lista(cod):
+    dep = D[cod]
+    idcad = dep["IDECADASTRO"]
+    pag = '<img src="/static/fotos/%s.jpg"/>'% (idcad)
+    pag += "<ul>"
+    for tag, valor in dep.items():
+        pag += "<li>%s: %s</li>" % (tag, valor)
+    pag += "</ul>"   
     return pag
 
 
